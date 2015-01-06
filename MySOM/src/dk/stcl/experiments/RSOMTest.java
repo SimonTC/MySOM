443a35f9ff6c1c73913420d90b40f6f1dbfc8dca
Simon <Simon@tcpost.dk>
2015-01-06 14:05:01 +0100

    Playing around with RSOM

--- MySOM/src/dk/stcl/experiments/RSOMTest.java
+++ MySOM/src/dk/stcl/experiments/RSOMTest.java
@@ -1,5 +1,6 @@
 package dk.stcl.experiments;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import dk.stcl.som.RSOM;
@@ -8,8 +9,11 @@ public class RSOMTest {
 	private Random rand = new Random(1234);
 	private RSOM rsom;
 	private final int NUM_ITERATIONS = 1000;
+	private final double DECAY = 0.7;
+	private final int SIZE = 3;
 	
-	private double[][] sequences;
+	private ArrayList<double[]> sequences;
+	
 
 	public static void main(String[] args) {
 		RSOMTest runner = new RSOMTest();
@@ -24,31 +28,23 @@ public class RSOMTest {
 	}
 	
 	private void buildSequences(){
-		sequences = new double[4][4];
+		sequences = new ArrayList<double[]>();
 		
-		sequences[0][0] = 0;
-		sequences[0][1] = 0;
-		sequences[0][2] = 1;
-		sequences[0][3] = 1;
+		double[] d1 = {0,0,1,1};
+		double[] d2 = {1,1,0,0};
+		double[] d3 = {1,0,1,0};
+		double[] d4 = {0,1,0,1};
+		double[] d5 = {0,0,1,1,1};
+		sequences.add(d1);
+		sequences.add(d2);
+		sequences.add(d3);
+		sequences.add(d4);
+		sequences.add(d5);
 		
-		sequences[1][0] = 1;
-		sequences[1][1] = 1;
-		sequences[1][2] = 0;
-		sequences[1][3] = 0;
-		
-		sequences[2][0] = 1;
-		sequences[2][1] = 1;
-		sequences[2][2] = 0;
-		sequences[2][3] = 1;
-		
-		sequences[3][0] = 0;
-		sequences[3][1] = 0;
-		sequences[3][2] = 1;
-		sequences[3][3] = 1;
 	}
 	
 	private void setupRSOM(){
-		rsom = new RSOM(2, 2, 1, rand, 0.1, 1, 0.7);
+		rsom = new RSOM(SIZE, SIZE, 1, rand, 0.1, (double) SIZE / 2, DECAY);
 	}
 	
 	public void train(){
