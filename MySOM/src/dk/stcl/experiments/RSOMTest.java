7aa95dfd7c4d2a38c8529b0cb79938a5e1b799d9
Simon <Simon@tcpost.dk>
2015-01-19 12:11:17 +0200

    Finished reiplemeting the RSOM test

--- MySOM/src/dk/stcl/experiments/RSOMTest.java
+++ MySOM/src/dk/stcl/experiments/RSOMTest.java
@@ -1,10 +1,12 @@
 package dk.stcl.experiments;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import dk.stcl.som.IRSOM;
 import dk.stcl.som.ISomBasics;
+import dk.stcl.som.containers.SomNode;
 import dk.stcl.som.online.rsom.RSOM;
 import dk.stcl.som.online.rsom.RSOMlo;
 
@@ -12,13 +14,20 @@ public class RSOMTest {
 	private Random rand = new Random(1234);
 	private IRSOM rsom;
 	private final int NUM_ITERATIONS = 1000;
-	private final double DECAY = 0.1;
+	private final double DECAY = 0.3;
 	private final int SIZE = 3;
 	private final boolean USE_SIMPLE_SEQUENCES = true;
 	private enum RSOMTYPES {RSOM,RSOMlo};
 	private final RSOMTYPES type = RSOMTYPES.RSOMlo; 
+	private HashMap<Integer, Integer> labelMap;
 	
 	private ArrayList<double[][]> sequences;
+	
+	
+	private final int INPUTLENGTH = 1;
+	private final int MAX_SEQUENCE_LENGTH = 3;
+	private final boolean DIFFERENT_LENGTH = true;
+	private final int NUM_SEQUENCES = 100;
 	
 
 	public static void main(String[] args) {
@@ -28,9 +37,20 @@ public class RSOMTest {
 	}
 
 	public void run(){
-		buildSequences(USE_SIMPLE_SEQUENCES);
-		setupRSOM();
-		train();
+		double fitness = 0;
+		for (int i = 0; i < 10; i++){
+			buildSequences(INPUTLENGTH, MAX_SEQUENCE_LENGTH, DIFFERENT_LENGTH, NUM_SEQUENCES);
+			setupRSOM();
+			train();
+			rsom.flush();
+			rsom.setLearning(false);
+			label();
+			rsom.flush();
+			fitness += validate();
+		}
+		fitness = fitness / 10;
+		System.out.println("Fitness: " + fitness);
+		
 	}
 	
 	private void buildSequences(boolean simple){
@@ -38,9 +58,75 @@ public class RSOMTest {
 			sequences = simpleSequences();
 		} else {
 			sequences = complexSequences();
+		}	
+	}
+	
+	private void buildSequences (int inputLength, int maxSequenceLength, boolean differentLength, int numSequences){
+		sequences = new ArrayList<double[][]>();
+		for (int seq = 0; seq < numSequences; seq++){
+			int length = 0;
+			if (differentLength){
+				length = rand.nextInt(maxSequenceLength) + 1;
+			} else {
+				length = maxSequenceLength;
+			}
+			double[][] sequence = new double[length][inputLength];
+			for (int part = 0; part < length; part++){
+				for (int i = 0; i < inputLength; i++){
+					sequence[part][i] = rand.nextInt(2);
+				}
+			}
+			sequences.add(sequence);
+		}
+	}
+	
+	private void label(){
+		int nextFreeLabel = 0;
+		int curLabel = -1;
+		labelMap = new HashMap<Integer, Integer>();
+		
+		for (double[][] seq : sequences){
+			int hash = toHash(seq);
+			SomNode bmu = doSequence(seq);
+			rsom.flush();
+			if (labelMap.containsKey(hash)){
+				curLabel = labelMap.get(hash);
+			} else {
+				curLabel = nextFreeLabel++;
+				labelMap.put(hash, curLabel);
+			}
+			
+			bmu.setLabel(curLabel);
+		}
+	}
+	
+	private double validate(){
+		int correct = 0;
+		int total = 0;
+		int curlabel = 0;
+		
+		for (double[][] seq : sequences){
+			total++;
+			int hash = toHash(seq);
+			SomNode bmu = doSequence(seq);
+			curlabel = labelMap.get(hash);
+			if (bmu.getLabel() == curlabel) correct++;	
+			rsom.flush();
 		}
 		
+		double fitness = (double) correct / total;
 		
+		return fitness;
+	}
+	
+	private int toHash(double[][] input){
+		String s = "";
+		for (double[] pair : input){
+			for (double d : pair){
+				s = s + (int) d;
+			}
+		}
+		return s.hashCode();
 	}
 	
 	private ArrayList<double[][]> simpleSequences(){
@@ -100,20 +186,22 @@ public class RSOMTest {
 	
 	public void train(){
 		for (int i = 1; i < NUM_ITERATIONS; i++){
-			System.out.println("-----------------------------");
-			System.out.println("Iteration: " + i);
+			//System.out.println("-----------------------------");
+			//System.out.println("Iteration: " + i);
 			//rsom.sensitize(i, NUM_ITERATIONS, true, false);
 			for (double[][] seq : sequences){
-				String s = "";
-				for (double[] d : seq){
-					double[] inputVector = d;
-					rsom.step(inputVector);
-					s += rsom.getBMU().getId() + " ";
-				}
-				System.out.println(s);
+				doSequence(seq);
 				rsom.flush();
 			}
 			
 		}
+	}
+	
+	private SomNode doSequence(double[][] seq){
+		SomNode bmu = null;
+		for (double[] d : seq){
+			bmu = rsom.step(d);
+		}
+		return bmu;
 	}
 }
