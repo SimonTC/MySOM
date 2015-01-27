3b5a3d1f8a93993918add6020e1a320ff4476e28
Simon <Simon@tcpost.dk>
2015-01-27 14:42:57 +0200

    Fix last problem

--- MySOM/src/dk/stcl/experiments/loopSOMFun/FourPoints.java
+++ MySOM/src/dk/stcl/experiments/loopSOMFun/FourPoints.java
@@ -79,7 +79,7 @@ public class FourPoints {
 		som.step(input);
 		
 		//Calculate activation
-		SimpleMatrix activation = som.computeActivationMatrix(true);
+		SimpleMatrix activation = som.computeActivationMatrix();
 		
 		//Give activation to RSOM
 		rsom.step(activation.getMatrix().data);
